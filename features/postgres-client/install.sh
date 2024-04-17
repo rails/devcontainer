d7f63ac13d5df9d72b0af9687d7c157dc7a6992c
Cadu Ribeiro <mail@cadu.dev>
2024-04-17 17:13:19 -0300

    Add postgresql-client to postgresql-client feature
    Currently when you generate an App with postgres and access the
    devcontainer, it fails to run `bin/rails db` inside the container
    terminal because there is no `psql` installed on it. This commit
    installs postgresql-client so it can be used inside the devcontainer.

--- features/postgres-client/install.sh
+++ features/postgres-client/install.sh
@@ -1,3 +1,3 @@
-apt-get update -y && apt-get -y install --no-install-recommends libpq-dev
+apt-get update -y && apt-get -y install --no-install-recommends libpq-dev postgresql-client
 
 rm -rf /var/lib/apt/lists/*
