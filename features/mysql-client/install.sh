86cf2ada57be7e553b78e887f9dc98a7ee598c93
Andrew Novoselac <andrew.novoselac@shopify.com>
2024-07-02 14:08:37 -0400

    mysql-client: Use default-mysql-client instead of default-libmysqlclient-dev
    Rails apps using structure.sql need to use mysqldump when setting up the db and running migrations. That is provided by  default-mysql-client.

--- features/mysql-client/install.sh
+++ features/mysql-client/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-apt-get update -y && apt-get -y install --no-install-recommends default-libmysqlclient-dev
+apt-get update -y && apt-get -y install --no-install-recommends default-mysql-client
 
 rm -rf /var/lib/apt/lists/*
