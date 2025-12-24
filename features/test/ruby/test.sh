8f14499af5232cfd61794219d677791d490c7f61
Rafael Mendonça França <rafael@rubyonrails.org>
2025-12-24 22:26:11 +0000

    Add Ruby 4.0.0

--- features/test/ruby/test.sh
+++ features/test/ruby/test.sh
@@ -8,6 +8,6 @@ check "mise is installed" bash -c "mise --version"
 check "mise init is sourced in the bashrc" bash -c "grep 'eval \"\$(~/.local/bin/mise activate bash)\"' $HOME/.bashrc"
 check "mise idiomatic version file is enabled for ruby" bash -c "mise settings | grep idiomatic_version_file_enable_tools | grep ruby"
 check "Ruby is installed with YJIT" bash -c "RUBY_YJIT_ENABLE=1 ruby -v | grep +YJIT"
-check "Ruby version is set to 3.4.8" bash -c "mise use -g ruby | grep 3.4.8"
+check "Ruby version is set to 4.0.0" bash -c "mise use -g ruby | grep 4.0.0"
 
 reportResults
