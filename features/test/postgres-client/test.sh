f3a25e2c5cf311b3af8137c84a7059886dcdb7e8
Lars Greiving <lars.greiving@finstreet.de>
2026-05-05 14:30:45 +0200

    Update postgres-client feature tests to account for version bump

--- features/test/postgres-client/test.sh
+++ features/test/postgres-client/test.sh
@@ -5,7 +5,7 @@ set -e
 source dev-container-features-test-lib
 
 check "PostgreSQL client is installed" bash -c "psql --version"
-check "PostgreSQL version is 15" bash -c "psql --version | grep -q 'psql (PostgreSQL) 15'"
+check "PostgreSQL version is 17" bash -c "psql --version | grep -q 'psql (PostgreSQL) 17'"
 check "libpq-dev is installed" bash -c "dpkg -l | grep libpq-dev"
 
 reportResults
--- features/test/postgres-client/version_18.sh
+++ features/test/postgres-client/version_18.sh
@@ -5,7 +5,7 @@ set -e
 source dev-container-features-test-lib
 
 check "PostgreSQL client is installed" bash -c "psql --version"
-check "PostgreSQL version is 14" bash -c "psql --version | grep -q 'psql (PostgreSQL) 14'"
+check "PostgreSQL version is 18" bash -c "psql --version | grep -q 'psql (PostgreSQL) 18'"
 check "libpq-dev is installed" bash -c "dpkg -l | grep libpq-dev"
 
 reportResults
